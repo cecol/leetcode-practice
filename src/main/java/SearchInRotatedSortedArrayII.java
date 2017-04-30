1a015d7a8d3e3843ba02e63abca96d1401fb248e
wilber chao <wilber.chao@vpon.com>
2017-04-30 17:44:46 +0800

    src/main/java/SearchInRotatedSortedArrayII.java

---
+++ src/main/java/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,16 @@
+/**
+ * lee code question address
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/#/description
+ * too easy
+ */
+public class SearchInRotatedSortedArrayII {
+    public static void main(String[] args) {
+    }
+
+    public boolean search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return true;
+        }
+        return false;
+    }
+}
