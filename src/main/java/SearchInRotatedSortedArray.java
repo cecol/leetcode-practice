d06471ea62f4d59965e92bf775efddb1c327573f
wilber chao <wilber.chao@vpon.com>
2017-04-30 17:34:11 +0800

    add SearchInRotatedSortedArray

---
+++ src/main/java/SearchInRotatedSortedArray.java
@@ -0,0 +1,16 @@
+/**
+ * lee code question address
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/#/description
+ * have nothing to talk about... too easy
+ */
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+    }
+
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return i;
+        }
+        return -1;
+    }
+}
