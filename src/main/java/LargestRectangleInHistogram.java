36f0a0a79043cc052ab9ffd0c1514b4bc18be415
wilber chao <wilber.chao@vpon.com>
2017-05-01 14:11:59 +0800

    finish LargestRectangleInHistogram

---
+++ src/main/java/LargestRectangleInHistogram.java
@@ -0,0 +1,73 @@
+/**
+ * lee code question address
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/#/description
+ */
+
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+        int[] c1 = {2, 1, 5, 6, 2, 3};
+        int[] c2 = {2, 0, 2};
+        int[] c3 = {0, 9};
+        int[] c4 = {2, 1, 2};
+        int[] c5 = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+        int[] c6 = {2, 2, 2, 2, 2, 2, 2, 2, 2, 100};
+        int[] c7 = {2, 1, 4, 5, 1, 3, 3};
+        int[] c8 = {1, 2, 3, 4, 5, 6, 7};
+        int[] c9 = {3, 6, 5, 7, 4, 8, 1, 0};
+        int[][] cc = {c1, c2, c3, c4, c5, c6, c7, c8, c9};
+        for (int i = 0; i < cc.length; i++) {
+            int r = largestRectangleArea(cc[i]);
+            System.out.println("case:" + (i + 1) + ", result: " + r);
+        }
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int max = 0;
+        int count = 0;
+        boolean isAscending = true;
+
+        if (heights.length == 0) return max;
+        if (heights.length == 1) return heights[0];
+        /**
+         * check for case: [1,2,3,4,5,6,7,8,9 ....]
+         * improve performance, this is BigO(n)
+         * */
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] == 0) continue;
+            if (i != heights.length - 1 && heights[i] > heights[i + 1]) {
+                isAscending = false;
+                break;
+            }
+            int sum = heights[i] * (heights.length - i);
+            if (sum > max) max = sum;
+        }
+
+        /**
+         * for normal case: [2, 1, 5, 6, 2, 3]
+         * you need to check each value, check left Histograms and ckeck right Histograms
+         * BigO(n^2)
+         * */
+        if (!isAscending) {
+            max = 0;
+            for (int i = 0; i < heights.length; i++) {
+                if (heights[i] == 0) continue;
+                //go left
+                for (int j = i - 1; j >= 0; j--) {
+                    if (heights[j] == 0 || heights[j] < heights[i]) break;
+                    else count += 1;
+                }
+                //go right
+                for (int j = i; j < heights.length; j++) {
+                    if (heights[j] == 0 || heights[j] < heights[i]) break;
+                    else count += 1;
+                    //for case [1,1,1,1,1,1,1,1....] can just jump to last
+                    if (heights[j] == heights[i] && j == i + 1) i = j;
+                }
+                int sum = heights[i] * count;
+                if (sum > max) max = sum;
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
