0cc2306436d19c7f786bd61a6729b718af161528
wilber chao <wilber.chao@vpon.com>
2017-05-12 00:07:58 +0800

    add MaximalSquare

--- src/main/java/MaximalRectangle.java
+++ src/main/java/MaximalRectangle.java
@@ -28,10 +28,10 @@ public class MaximalRectangle {
     /**
      * height: height for x-th rows
      * idx: row position
-     * */
+     */
     public static void restHeight(char[][] matrix, int[] height, int idx) {
         for (int i = 0; i < matrix[0].length; i++) {
-            if(matrix[idx][i] == '1') height[i] += 1;
+            if (matrix[idx][i] == '1') height[i] += 1;
             else height[i] = 0; //if that char is '0', we don't need to care about area, cuz of it is calculated previous row
             //and if next row is '1', it need to restart from 1
         }
