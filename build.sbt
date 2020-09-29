4f0f3cfd66250a1ac17dbc1a3ca01aa29fdd7bf8
wilber <cecol3500123@gmail.com>
2020-09-29 14:18:33 +0800

    add LC1512

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "lee-code-practice"
 version := "1.0"
 
 scalaVersion := "2.12.2"
-    
+
+libraryDependencies ++= Seq("ch.qos.logback"             % "logback-classic"          % "1.2.3")
---
+++ src/main/java/leetcode202009/easy/LC1512NumberOfGoodPairs.java
@@ -0,0 +1,44 @@
+package leetcode202009.easy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+
+public class LC1512NumberOfGoodPairs {
+  public static Logger logger = LoggerFactory.getLogger("LC1512NumberOfGoodPairs");
+  public static void main(String[] args) {
+    logger.info("result: {}", numIdenticalPairs2(new int[]{1,1,1,1}));
+  }
+
+  public static int numIdenticalPairs(int[] nums) {
+    HashMap<Integer, Integer> h = new HashMap<>();
+    for (int i : nums) {
+      if (h.containsKey(i)) {
+        int v = h.remove(i);
+        h.put(i, v + 1);
+      } else {
+        h.put(i, 1);
+      }
+    }
+    int s = 0;
+    for (Integer i : h.values()) s+=(i-1)*i/2;
+
+    return s;
+  }
+
+  public static int numIdenticalPairs2(int[] nums) {
+    HashMap<Integer, Integer> h = new HashMap<>();
+    int s = 0;
+    for (int i : nums) {
+      if (h.containsKey(i)) {
+        int v = h.remove(i);
+        h.put(i, v + 1);
+      } else {
+        h.put(i, 1);
+      }
+      s += h.get(i) - 1;
+    }
+    return s;
+  }
+}
