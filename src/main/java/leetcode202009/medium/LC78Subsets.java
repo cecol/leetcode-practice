a820b078aabf429ada2016984cf766fc2cc73dd9
wilber <cecol3500123@gmail.com>
2020-09-30 23:09:44 +0800

    add LC78 more solutions

--- src/main/java/leetcode202009/medium/LC78Subsets.java
+++ src/main/java/leetcode202009/medium/LC78Subsets.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class LC78Subsets {
     public static void main(String[] args) {
-        var r = subsets(new int[]{1, 2, 3});
+        var r = subsets3(new int[]{1, 2, 3});
         for (List<Integer> i : r) System.out.println(i.stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
@@ -26,5 +26,46 @@ public class LC78Subsets {
             a.add(g);
             getSet(new ArrayList<>(x), new HashSet<>(a), r);
         }
+    }
+
+    public static List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> r = new ArrayList<>();
+        List<Integer> x = new ArrayList<>();
+        r.add(new ArrayList<>());
+        getSet2(nums, 0, x, r);
+        return r;
+    }
+
+    /**
+     * recursive case
+     * */
+    public static void getSet2(int[] nums, int offset, List<Integer> x, List<List<Integer>> r) {
+        if (offset == nums.length) {
+            return;
+        } else {
+            x.add(nums[offset]);
+            r.add(new ArrayList<>(x));
+            getSet2(nums, offset + 1, x, r);
+            x.remove(x.size() - 1);
+            getSet2(nums, offset + 1, x, r);
+        }
+    }
+
+    /**
+     * iteration case
+     * */
+    public static List<List<Integer>> subsets3(int[] nums) {
+        List<List<Integer>> r = new ArrayList<>();
+        r.add(new ArrayList<>());
+        for (int i : nums) {
+            List<List<Integer>> n = new ArrayList<>();
+            for (List<Integer> l : r) {
+                List<Integer> cp = new ArrayList<>(l);
+                cp.add(i);
+                n.add(cp);
+            }
+            r.addAll(n);
+        }
+        return r;
     }
 }
