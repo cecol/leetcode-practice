dae4bcd579e714ee5d180eb87fbfdefdcacae774
wilber <cecol3500123@gmail.com>
2020-10-08 17:18:07 +0800

    Update LC518

--- src/main/java/leetcode202009/medium/LC518CoinChange2.java
+++ src/main/java/leetcode202009/medium/LC518CoinChange2.java
@@ -2,24 +2,31 @@ package leetcode202009.medium;
 
 import leetcode202009.BasicTemplate;
 
+import java.util.Arrays;
+
 public class LC518CoinChange2 extends BasicTemplate {
 
-    public static void main(String[] args) {
-        var LC = new LC518CoinChange2();
-        var r = LC.change(5, new int[]{1, 2, 5});
-    }
+  public static void main(String[] args) {
+    var LC = new LC518CoinChange2();
+    var r = LC.change(5, new int[]{1, 2, 5});
+  }
 
-    public int change(int amount, int[] coins) {
-        int[] x = new int[amount + 1];
-        x[0] = 1;
-        for (int i = 1; i < x.length; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0) {
-                    x[i] += x[i - coins[j]];
-                }
-            }
+  public int change(int amount, int[] coins) {
+    int[][] x = new int[coins.length][amount + 1];
+    Arrays.sort(coins);
+    log.debug("{}", coins);
+    for (int i = 0; i < coins.length; i++) x[i][0] = 1;
+    for (int i = 0; i < x.length; i++) {
+      for (int j = 1; j < x[i].length; j++) {
+        if (j >= coins[i]) {
+          int uMax = i > 0 ? x[i - 1][j] : 0;
+          int pMax = x[i][j - coins[i]];
+          log.debug("{}, {}, {}, {}");
+          x[i][j] = uMax + pMax;
         }
-        log.debug("{}", x);
-        return x[amount];
+      }
     }
+    for (int[] p : x) log.debug("{}", p);
+    return x[coins.length - 1][amount];
+  }
 }
