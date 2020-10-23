2f5cedf41159fccd6b79ef5569f0d61ce1220939
wilber <cecol3500123@gmail.com>
2020-10-23 21:28:43 +0800

    add optimize 443

--- src/main/java/leetcode202009/medium/LC443StringCompression.java
+++ src/main/java/leetcode202009/medium/LC443StringCompression.java
@@ -6,7 +6,7 @@ public class LC443StringCompression extends BasicTemplate {
 
     public static void main(String[] args) {
         var LC = new LC443StringCompression();
-        var r = LC.compress(new char[]{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'});
+        var r = LC.compress2(new char[]{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'});
     }
 
     public int compress(char[] chars) {
@@ -30,5 +30,27 @@ public class LC443StringCompression extends BasicTemplate {
         for (int i = 0; i < rs.length(); i++) chars[i] = rs.charAt(i);
         log.debug("{}", chars);
         return rs.length();
+    }
+
+    public int compress2(char[] chars) {
+        if (chars == null || chars.length == 0) return 0;
+        if (chars.length == 1) return 1;
+        int index = 0;
+        int i = 0;
+        while (i < chars.length) {
+            int j = i;
+            while (j < chars.length && chars[i] == chars[j]) j++;
+            int size = j - i;
+            chars[index++] = chars[i];
+            if (size == 1) {
+            } else if (size < 10) {
+                chars[index++] = (char) ('0' + size);
+            } else {
+                char[] sc = String.valueOf(size).toCharArray();
+                for (char c : sc) chars[index++] = c;
+            }
+            i = j;
+        }
+        return index;
     }
 }
