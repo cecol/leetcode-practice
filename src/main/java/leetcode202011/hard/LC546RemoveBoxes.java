0c8cb90af956a682796b17555c459fefead2923a
wilber <cecol3500123@gmail.com>
2020-11-24 23:57:29 +0800

    Add 546

---
+++ src/main/java/leetcode202011/hard/LC546RemoveBoxes.java
@@ -0,0 +1,14 @@
+package leetcode202011.hard;
+
+import leetcode20200921to20201031.BasicTemplate;
+
+public class LC546RemoveBoxes extends BasicTemplate {
+    public static void main(String[] args) {
+        var LC = new LC546RemoveBoxes();
+        var s = LC.removeBoxes(new int[]{1, 3, 2, 2, 2, 3, 4, 3, 1});
+    }
+
+    public int removeBoxes(int[] boxes) {
+
+    }
+}
