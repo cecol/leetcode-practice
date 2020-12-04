8b004436a7f9759da510a3b552e0011736d48fcd
wilber <cecol3500123@gmail.com>
2020-12-05 00:09:03 +0800

    update 96

--- src/main/java/leetcode202011/medium/LC96UniqueBinarySearchTrees.java
+++ src/main/java/leetcode202011/medium/LC96UniqueBinarySearchTrees.java
@@ -3,41 +3,33 @@ package leetcode202011.medium;
 import leetcode20200921to20201031.BasicTemplate;
 
 public class LC96UniqueBinarySearchTrees extends BasicTemplate {
-  public static void main(String[] args) {
-    var LC = new LC96UniqueBinarySearchTrees();
+    public static void main(String[] args) {
+        var LC = new LC96UniqueBinarySearchTrees();
 //    var s = LC.numTrees(3);
-    var s2 = LC.numTrees(6);
-  }
-
-  public int numTrees(int n) {
-    if (n <= 1) return n;
-    int[][] dp = new int[n + 1][n + 1];
-    int res = countTree("", dp, 1, n);
-    logIntArray(dp);
-    log.debug("res: {}", res);
-    return res;
-  }
-
-  public int countTree(String tab, int[][] dp, int begin, int end) {
-    log.debug(tab + "countTree begin: {}, end: {}", begin, end);
-    if (begin >= end) return 0;
-    if (end >= dp.length) return 0;
-    if (dp[begin][end] != 0) return dp[begin][end];
-    if (end - begin == 1) {
-      dp[begin][end] = 2;
-      return dp[begin][end];
+        var s2 = LC.numTrees(3);
     }
 
-    int res = 0;
-    for (int root = begin; root <= end; root++) {
-      int left = countTree(tab + "\t", dp, begin, root - 1);
-      int right = countTree(tab + "\t", dp, root + 1, end);
-      if (left == right && left == 0) res++;
-      else if (left <= 1 || right <= 1) res += (left + right);
-      else res += left * right;
-      log.debug(tab + "countTree root: {}, begin: {}, end: {}, left: {}, right: {}, res: {}", root, begin, end, left, right, res);
+    public int numTrees(int n) {
+        int[][] dp = new int[n][n];
+        var res = ct("", 0, n - 1, dp);
+        log.debug("{}", res);
+        return res;
     }
-    dp[begin][end] = res;
-    return res;
-  }
+
+    public int ct(String tab, int b, int e, int[][] dp) {
+        log.debug("{}b:{},e:{}", tab, b, e);
+        if (e - b == 1) return 2;
+        if (e == b) return 1;
+        if (dp[b][e] != 0) return dp[b][e];
+
+        int res = 0;
+        for (int i = b; i <= e; i++) {
+            log.debug("{}i:{}", tab, i);
+            if (b == i) res += ct(tab + "\t", i + 1, e, dp);
+            else if (e == i) res += ct(tab + "\t", b, i - 1, dp);
+            else res += ct(tab + "\t", b, i - 1, dp) * ct(tab + "\t", i + 1, e, dp);
+        }
+        dp[b][e] = res;
+        return res;
+    }
 }
