3939ec65718a54da0660a35df0bb0620ecd7c76e
wilber <cecol3500123@gmail.com>
2021-01-12 17:06:23 +0800

    Add 743, 310

---
+++ src/main/java/leetcode202101/medium/LC310MinimumHeightTrees.java
@@ -0,0 +1,55 @@
+package leetcode202101.medium;
+
+import leetcode20200921to20201031.BasicTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class LC310MinimumHeightTrees extends BasicTemplate {
+  public static void main(String[] args) {
+    Logger log = LoggerFactory.getLogger("Main");
+    var LC = new LC310MinimumHeightTrees();
+    var s = LC.findMinHeightTrees(4, new int[][]{{1, 0}, {1, 2}, {1, 3}});
+    var s2 = LC.findMinHeightTrees(6, new int[][]{{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4}});
+  }
+
+  /**
+   * https://leetcode.com/problems/minimum-height-trees/discuss/76055/Share-some-thoughts
+   * Tree: 沒有cycle的 graph, 所以可以形成tree的graph一定是no cycle
+   * graph中可以有多個最長路徑, 但他們之間一定共享同組中間點, ((如果不共享, Height & 最長路徑會再延長(不然就不聯通)
+   * -   簡單來說
+   * -   max length = 奇數 -> 只有1個中間點, 形成 Min Height
+   * -   max length = 偶數 -> 只有2個中間點, 形成 Min Height
+   * 基本概念就是從leave(degree == 1)出發,過程中就找到parent
+   * (不從leave開始找, 就是把leave當作的tree root,一定不會是Min Height, 因為leave當root會造成longest path -> Max height)
+   * 每拿到leave就把node size -= 1
+   * 把parent的到leave中間的edge拆掉, 這時候parent可能就成為 new leave,
+   * 然後再把new leave放到leaves繼續下去找,
+   * 直到node size < 2 因為只有中間點的1 or 2 node可以當作root來達成Min Height
+   *
+   * 所以就是透過 BFS 從leave出發, 過程中邊砍leave 如果剩下的節點少於2, 必然就是root
+   * */
+  public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+    if (n == 1) return Collections.singletonList(0);
+    Set<Integer>[] nodeEdges = new HashSet[n];
+    for (int i = 0; i < n; i++) nodeEdges[i] = new HashSet<>();
+    for (int[] edge : edges) {
+      nodeEdges[edge[0]].add(edge[1]);
+      nodeEdges[edge[1]].add(edge[0]);
+    }
+    List<Integer> leaves = new ArrayList<>();
+    for (int i = 0; i < n; i++) if (nodeEdges[i].size() == 1) leaves.add(i);
+    while (n > 2) {
+      n -= leaves.size();
+      List<Integer> newLeaves = new ArrayList<>();
+      for (int l : leaves) {
+        int parent = nodeEdges[l].iterator().next();
+        nodeEdges[parent].remove(l);
+        if (nodeEdges[parent].size() == 1) newLeaves.add(parent);
+      }
+      leaves = newLeaves;
+    }
+    return leaves;
+  }
+}
--- src/main/java/leetcode202101/medium/LC743NetworkDelayTime.java
+++ src/main/java/leetcode202101/medium/LC743NetworkDelayTime.java
@@ -4,20 +4,103 @@ import leetcode20200921to20201031.BasicTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class LC743NetworkDelayTime extends BasicTemplate {
-    public static void main(String[] args) {
-        Logger log = LoggerFactory.getLogger("Main");
-        var LC = new LC743NetworkDelayTime();
-        var s = LC.networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2);
+  public static void main(String[] args) {
+    Logger log = LoggerFactory.getLogger("Main");
+    var LC = new LC743NetworkDelayTime();
+    var s = LC.networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2);
+  }
+
+  /**
+   * 這題Dijkstra minimum-BFS走完, 最後一個走到的點的shortest path cost(Max value)就是總共要花的時間
+   * Shortest path經典題, 題目核心: 從一個點K開始, 找到到其他點最短路徑
+   * Dijkstra's algorithm 基本概念:從起始點下去, 找最小路徑擴散(minimum-BFS)到其他點, 再由其他已知shortest path的點擴散下去
+   *       create vertex set Q                     -> 記錄尚未確定為shortest path的點
+   *
+   *       for each vertex v in Graph:             -> 設定每個點距離的初始值: MAX_VALUE
+   *           dist[v] ← INFINITY                  -> 起始點到每個點已計算的距離
+   *           prev[v] ← UNDEFINED
+   *           add v to Q                          -> 先加入到還沒走過的點
+   *       dist[source] ← 0                        -> 起始點到自己的距離 = 0
+   *
+   *       while Q is not empty:                   -> 還有點還沒計算到
+   *           u ← vertex in Q with min dist[u]    -> 拿出尚在計算shortest pat的點中已知最小值
+   *
+   *           remove u from Q                     -> 把他移出(因為他是最小值, 他已經是shortest path,
+   *                                                   而是要看能否從他在走道其他點造成下一次的shortest pat)
+   *
+   *           for each neighbor v of u:           -> 就是上面說的, 已經是確認shortest pat的u, 找出他能走到的點,
+   *                                                  看能否再次更新shortest path的紀錄
+   *               alt ← dist[u] + length(u, v)
+   *               if alt < dist[v]:               -> 確實透過這個點u, 可以更新shortest path到其他點
+   *                   dist[v] ← alt
+   *                   prev[v] ← u
+   *
+   * 所以這題的K 就是起始點, int[][] times裡面的(u, v, w)中的w是weight, 就是到那個u -> v的path cost
+   * 所以先建立 Map<Integer, Map<Integer, Integer>> path, 每個點到其他點的path costs
+   * - key 是 u, value是 u到其他點v的 costs, 一個u到v可能有多條path, 只記錄下minimum path
+   * Map<Integer, Integer> kToOthersDistance = new HashMap<>(); 就是紀錄起點K到其他點的distance
+   * 最後跑完就會是 shortest path
+   *
+   * PriorityQueue<int[]> shortestPathMinHeapBfs = new PriorityQueue<>(Comparator.comparingInt(i -> i[1]));
+   * 就是用來放起點K到其他點的所有距離, PriorityQueue是讓你直接拿到最短那個繼續下去算
+   * 所以裡面先放起始點 [K,0], 代表從K出發, 開始找其他shortest path, 而且是由最小的來往後找, 所以是由最小點開始BFS -> minimum BFS
+   *
+   * 這題Dijkstra minimum-BFS走完, 最後一個走到的點的shortest path(Max value)就是總共要花的時間
+   * 如果最後走完的點數量跟實際不樣, 代表有點走不到 -> -1
+   *
+   * 時間複雜度 Time complexity: O(Nlog(N) + E), Space complexity: O(N + E)
+   * 其中Nlog(N)是因為用PriorityQueue來找minimum cost, 用heap tree有加速
+   * 其實還可以用以下解, 但時間複雜度沒有 Dijkstra + BFS Heap來的快
+   * Floyd-Warshall - Time complexity: O(N^3), Space complexity: O(N^2)
+   * Bellman-Ford解 - Time complexity: O(N*E), Space complexity: O(N)
+   * 所以沒有細讀
+   * 可參考
+   * https://leetcode.com/problems/network-delay-time/discuss/183873/Java-solutions-using-Dijkstra-FloydWarshall-and-Bellman-Ford-algorithm
+   *
+   * 原始解法參考
+   * https://leetcode.com/problems/network-delay-time/discuss/109968/Simple-JAVA-Djikstra's-(PriorityQueue-optimized)-Solution-with-explanation
+   * */
+  public int networkDelayTime(int[][] times, int N, int K) {
+    if (times == null || times.length == 0) return -1;
+    Map<Integer, Map<Integer, Integer>> path = new HashMap<>();
+    for (int[] time : times) {
+      Map<Integer, Integer> destM = path.get(time[0]);
+      if (destM == null) {
+        destM = new HashMap<>();
+        path.put(time[0], destM);
+      }
+      Integer distance = destM.get(time[1]);
+      if (distance == null || distance > time[2]) {
+        destM.put(time[1], time[2]);
+      }
     }
 
-    public int networkDelayTime(int[][] times, int N, int K) {
+    Map<Integer, Integer> kToOthersDistance = new HashMap<>();
+    kToOthersDistance.put(K, 0);
+    PriorityQueue<int[]> shortestPathMinHeapBfs = new PriorityQueue<>(Comparator.comparingInt(i -> i[1]));
+    shortestPathMinHeapBfs.offer(new int[]{K, 0});
+    int max = -1;
+    while (!shortestPathMinHeapBfs.isEmpty()) {
+      int[] nextSPBfs = shortestPathMinHeapBfs.poll();
+      int SPNode = nextSPBfs[0];
+      int SPDist = nextSPBfs[1];
 
+      if (kToOthersDistance.containsKey(SPNode) && kToOthersDistance.get(SPNode) < SPDist) continue;
+      Map<Integer, Integer> SPNodeToOther = path.get(SPNode);
+      if (SPNodeToOther == null) continue;
+      for (Map.Entry<Integer, Integer> nextNode : SPNodeToOther.entrySet()) {
+        int nextDist = SPDist + nextNode.getValue();
+        int nextNodeV = nextNode.getKey();
+        if (kToOthersDistance.containsKey(nextNodeV) && kToOthersDistance.get(nextNodeV) <= nextDist) continue;
+        kToOthersDistance.put(nextNodeV, nextDist);
+        shortestPathMinHeapBfs.offer(new int[]{nextNodeV, nextDist});
+      }
     }
+    for (int val : kToOthersDistance.values()) if (val > max) max = val;
+    return kToOthersDistance.size() == N ? max : -1;
+  }
 }
