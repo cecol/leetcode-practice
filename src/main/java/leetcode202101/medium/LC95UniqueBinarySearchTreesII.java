316a0e4dbac0ae479f3941f49d2c5d1515fcd237
wilber <cecol3500123@gmail.com>
2021-02-23 17:25:31 +0800

    update tree 95

--- src/main/java/leetcode202101/medium/LC95UniqueBinarySearchTreesII.java
+++ src/main/java/leetcode202101/medium/LC95UniqueBinarySearchTreesII.java
@@ -9,44 +9,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LC95UniqueBinarySearchTreesII extends BasicTemplate {
-    public static void main(String[] args) {
-        Logger log = LoggerFactory.getLogger("Main");
-        var LC = new LC95UniqueBinarySearchTreesII();
-        var s = LC.generateTrees(3);
-    }
+  public static void main(String[] args) {
+    Logger log = LoggerFactory.getLogger("Main");
+    var LC = new LC95UniqueBinarySearchTreesII();
+    var s = LC.generateTrees(3);
+  }
 
-    /**
-     * https://leetcode.com/problems/unique-binary-search-trees-ii/discuss/31508/Divide-and-conquer.-F(i)-G(i-1)-*-G(n-i)
-     * 這題其實是DP 96的把所有可能的tree都畫出來
-     * 但我不知道怎透過96的過程把tree建立出來直到看到這個解法才恍然大悟
-     *
-     * 基本概念就是96的i from 1 to n
-     * 分別遞迴下去建立各種leftSubs & rightSubs
-     * 然後雙重迴圈分別取出每一個leftSub與rightSub交錯與當前的root i 建立回真正的tree
-     * */
-    public List<TreeNode> generateTrees(int n) {
-        return generateSubTrees(1, n);
-    }
+  /**
+   * https://leetcode.com/problems/unique-binary-search-trees-ii/discuss/31508/Divide-and-conquer.-F(i)-G(i-1)-*-G(n-i)
+   * 這題其實是DP 96的把所有可能的tree都畫出來
+   * 但我不知道怎透過96的過程把tree建立出來直到看到這個解法才恍然大悟
+   * <p>
+   * 基本概念就是96的i from 1 to n
+   * 分別遞迴下去建立各種leftSubs & rightSubs
+   * 然後雙重迴圈分別取出每一個leftSub與rightSub交錯與當前的root i 建立回真正的tree
+   */
+  public List<TreeNode> generateTrees(int n) {
+    return generateSubTrees(1, n);
+  }
 
-    private List<TreeNode> generateSubTrees(int s, int e) {
-        List<TreeNode> res = new ArrayList<>();
-        if (s > e) {
-            res.add(null);
-            return res;
+  private List<TreeNode> generateSubTrees(int s, int e) {
+    List<TreeNode> res = new ArrayList<>();
+    if (s > e) {
+      res.add(null);
+      return res;
+    }
+    for (int i = s; i <= e; i++) {
+      List<TreeNode> leftSub = generateSubTrees(s, i - 1);
+      List<TreeNode> rightSub = generateSubTrees(i + 1, e);
+      for (TreeNode left : leftSub) {
+        for (TreeNode right : rightSub) {
+          res.add(new TreeNode(i, left, right));
         }
-        for (int i = s; i <= e; i++) {
-            List<TreeNode> leftSub = generateSubTrees(s, i - 1);
-            List<TreeNode> rightSub = generateSubTrees(i + 1, e);
-            for(TreeNode left:leftSub) {
-                for(TreeNode right:rightSub) {
-                    TreeNode r = new TreeNode(i);
-                    r.left=left;
-                    r.right=right;
-                    res.add(r);
-                }
-            }
-
-        }
-        return res;
+      }
     }
+    return res;
+  }
 }
