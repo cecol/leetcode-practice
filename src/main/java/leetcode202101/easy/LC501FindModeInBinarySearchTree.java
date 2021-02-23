b7e14d9527a42f4ed427d2dc820561f0adbb7e67
wilber <cecol3500123@gmail.com>
2021-02-23 22:53:02 +0800

    fix tree 501, 437

--- src/main/java/leetcode202101/easy/LC501FindModeInBinarySearchTree.java
+++ src/main/java/leetcode202101/easy/LC501FindModeInBinarySearchTree.java
@@ -4,63 +4,63 @@ import leetcode20200921to20201031.BasicTemplate;
 import util.TreeNode;
 
 public class LC501FindModeInBinarySearchTree extends BasicTemplate {
-  public static void main(String[] args) {
-    var LC = new LC501FindModeInBinarySearchTree();
-    var s = LC.findMode(null);
-  }
-
-  /**
-   * 這題easy卻花了一堆時間QQ
-   * 這題是蠻好解的 -> 用一個 HashMap走過這棵樹就解了
-   * 主要是題目後面有要求Space O(1) -> 不能HashMap
-   * 後來找了很多答案雖然是聲稱O(1), 但實際上不是
-   * 有看到這一篇
-   * https://leetcode.com/problems/find-mode-in-binary-search-tree/discuss/98101/Proper-O(1)-space
-   * 看起來是先中序走過一遍 -> 中序中間處理root.val就是拿去算目前的count, 如果有大於 maxCount -> 更新 maxCount & modeCount
-   * -> 其中看看當前處理的curVal(就是root.val)是否有改變, 有改變就reset count
-   * (我覺得概念跟687很像, 只是這邊是root.val跟走過的左子樹不一樣 -> reset, 687是root.val跟左子樹不一樣, 以root來算+left都是0)
-   * 先確認有多少modes -> maxCount == curCount 來累加modeCount(但如果下次有踩到curCount > maxCount, 會reset)
-   * -> 然後initial int[] modes
-   * 在中序走過一遍把 int[] modes填滿
-   *
-   * 我在想為什麼得走中序(我試跑過前序 -> 失敗)
-   * 我想因為中序就是sorted order -> 往後面走都知道前面都是小於等於自己 -> 不會出現大於自己
-   * 所以只要拿當前val 去算當前max , modeCount就足矣
-   */
-  int curVal;
-  int curCount = 0;
-  int maxCount = 0;
-  int modeCount = 0;
-  int[] modes = null;
-
-  public int[] findMode(TreeNode root) {
-    inorder(root);
-    modes = new int[modeCount];
-    modeCount =0;
-    curCount=0;
-    inorder(root);
-    return modes;
-  }
-
-  private void handleValue(int i) {
-    if (i != curVal) {
-      curVal = i;
-      curCount = 0;
+    public static void main(String[] args) {
+        var LC = new LC501FindModeInBinarySearchTree();
+        var s = LC.findMode(null);
     }
-    curCount++;
-    if (curCount > maxCount) {
-      maxCount = curCount;
-      modeCount = 1;
-    } else if (maxCount == curCount) {
-      if (modes != null) modes[modeCount] = curVal;
-      modeCount++;
-    }
-  }
 
-  private void inorder(TreeNode r) {
-    if (r == null) return;
-    handleValue(r.val);
-    inorder(r.left);
-    inorder(r.right);
-  }
+    /**
+     * 這題easy卻花了一堆時間QQ
+     * 這題是蠻好解的 -> 用一個 HashMap走過這棵樹就解了
+     * 主要是題目後面有要求Space O(1) -> 不能HashMap
+     * 後來找了很多答案雖然是聲稱O(1), 但實際上不是
+     * 有看到這一篇
+     * https://leetcode.com/problems/find-mode-in-binary-search-tree/discuss/98101/Proper-O(1)-space
+     * 看起來是先中序走過一遍 -> 中序中間處理root.val就是拿去算目前的count, 如果有大於 maxCount -> 更新 maxCount & modeCount
+     * -> 其中看看當前處理的curVal(就是root.val)是否有改變, 有改變就reset count
+     * (我覺得概念跟687很像, 只是這邊是root.val跟走過的左子樹不一樣 -> reset, 687是root.val跟左子樹不一樣, 以root來算+left都是0)
+     * 先確認有多少modes -> maxCount == curCount 來累加modeCount(但如果下次有踩到curCount > maxCount, 會reset)
+     * -> 然後initial int[] modes
+     * 在中序走過一遍把 int[] modes填滿
+     * <p>
+     * 我在想為什麼得走中序(我試跑過前序 -> 失敗)
+     * 我想因為中序就是sorted order -> 往後面走都知道前面都是小於等於自己 -> 不會出現大於自己
+     * 所以只要拿當前val 去算當前max , modeCount就足矣
+     */
+    int curVal;
+    int curCount = 0;
+    int maxCount = 0;
+    int modeCount = 0;
+    int[] modes = null;
+
+    public int[] findMode(TreeNode root) {
+        inorder(root);
+        modes = new int[modeCount];
+        modeCount = 0;
+        curCount = 0;
+        inorder(root);
+        return modes;
+    }
+
+    private void handleValue(int i) {
+        if (i != curVal) {
+            curVal = i;
+            curCount = 0;
+        }
+        curCount++;
+        if (curCount > maxCount) {
+            maxCount = curCount;
+            modeCount = 1;
+        } else if (maxCount == curCount) {
+            if (modes != null) modes[modeCount] = curVal;
+            modeCount++;
+        }
+    }
+
+    private void inorder(TreeNode r) {
+        if (r == null) return;
+        inorder(r.left);
+        handleValue(r.val);
+        inorder(r.right);
+    }
 }
--- src/main/java/leetcode202101/medium/LC437PathSumIII.java
+++ src/main/java/leetcode202101/medium/LC437PathSumIII.java
@@ -5,9 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.TreeNode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class LC437PathSumIII extends BasicTemplate {
@@ -37,27 +35,32 @@ public class LC437PathSumIII extends BasicTemplate {
      * https://leetcode.com/problems/path-sum-iii/discuss/91878/17-ms-O(n)-java-Prefix-sum-method
      * 用HashMap版本 -> 紀錄已加總過的pre sum數值次數
      * 很像Two sum
-     * 每個點就是要紀錄從root到自己的加總過程 -> 從root到該點至有一條路徑 紀錄該路徑的two sum加總可能
-     * 所以到該點可以透過找 preSum.getOrDefault(target - cur, 0);
+     * 每個點就是要紀錄從root到自己的加總過程 -> 從root到該點只有一條路徑 紀錄該路徑的two sum加總可能
+     * 所以到該點可以透過找 preSum.getOrDefault(cur - target, 0);
      * 如果有的話 -> 一定是, 此路徑中, 中間某一點開始加總到目前的點剛好是target
-     * 就是說 target - cur = x 的值代表是否存在過此值在過程中出現過  -> target = x + cur -> 如果有x,
+     * 就是說 cur - target = x 的值代表是否存在過此值在過程中出現過
+     * ->   ex 10 -> 5 -> 8 -> 7 -> 6
+     * ->   走到6的cur 會經歷 10 -> 15 -> 23 -> 30 -> 36
+     * ->   如果要的 target = 20 => 30 - 10
+     * ->   所以就要檢查是否出現過10, 這個10就是由 30(cur) - 20(target) 得到的
+     * ->   代表在累加過程中有有出現過加總是 10, 再配上後面加總是30, 代表中間有一段加總是20 就是target
      * 代表之前累加過程中就有算過x(因此之後才又達到cur, cur是一定有的, map又是存cur之前出現的加總過程, 有可能有x有可能沒有,
      * -> 但是如果有x就代表從x 到 cur 就是 target)
-     *
      */
     public int pathSum2(TreeNode root, int sum) {
         Map<Integer, Integer> m = new HashMap<>();
-        return checkAllPossibility(m, root, 0, sum);
+        m.put(0, 1);
+        return checkByPreSum(m, root, 0, sum);
     }
 
-    private int checkAllPossibility(Map<Integer, Integer> preSum, TreeNode root, int cur, int target) {
+    private int checkByPreSum(Map<Integer, Integer> preSumCount, TreeNode root, int curSum, int target) {
         if (root == null) return 0;
-        cur += root.val;
-        int res = preSum.getOrDefault(target - cur, 0);
-        preSum.put(cur, preSum.getOrDefault(cur, 0) + 1);
-        res += checkAllPossibility(preSum, root.left, cur, target) +
-                checkAllPossibility(preSum, root.right, cur, target);
-        preSum.put(cur, preSum.get(cur) + 1);
-        return res;
+        curSum += root.val;
+        int middleSumCount = preSumCount.getOrDefault(curSum - target, 0);
+        preSumCount.put(curSum, preSumCount.getOrDefault(curSum, 0) + 1);
+        middleSumCount += checkByPreSum(preSumCount, root.left, curSum, target) +
+                checkByPreSum(preSumCount, root.right, curSum, target);
+        preSumCount.put(curSum, preSumCount.get(curSum) - 1);
+        return middleSumCount;
     }
 }
